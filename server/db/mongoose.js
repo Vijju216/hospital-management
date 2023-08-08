aabe7dbebee1349112c5005f6ca0d6af0aa087b9
Vijju216 <125346609+Vijju216@users.noreply.github.com>
2023-08-08 10:21:31 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,6 +4,6 @@ var mongoose = require ('mongoose');
 mongoose.Promise = global.Promise;
 
 //change the database with yours
-mongoose.connect("mongodb://admin:admin123@ds145220.mlab.com:45220/nhs-app");
+mongoose.connect("mongodb+srv://paligalavijay55:wjtLVOnprZMhuuwP@cluster0.he3hjai.mongodb.net/?retryWrites=true&w=majority");
 
 module.exports = {mongoose};
