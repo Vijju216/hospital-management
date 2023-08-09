f39189b3ef88db42342a8e370ee3408403cb4db8
Vijju216 <125346609+Vijju216@users.noreply.github.com>
2023-08-09 09:44:09 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // Change the database connection URL with yours
-mongoose.connect('mongodb+srv://paligalavijay55:wjtLVOnprZMhuuwP@cluster0.he3hjai.mongodb.net/?retryWrites=true&w=majority', {
+mongoose.connect("mongodb+srv://paligalavijay55:wjtLVOnprZMhuuwP@cluster0.he3hjai.mongodb.net/?retryWrites=true&w=majority", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
