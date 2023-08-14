70226e6d6d6497544cd3628fe5d43d6f669749bf
Vijju216 <125346609+Vijju216@users.noreply.github.com>
2023-08-14 10:17:42 +0530

    Update mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,15 @@
-var mongoose = require ('mongoose');
-
+var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-//change the database with yours
-mongoose.connect("mongodb+srv://paligalavijay55:y1yBLDbRIYklYrZh@cluster0.dojhzeo.mongodb.net/?retryWrites=true&w=majority");
+// Change the database connection URL with yours
+mongoose.connect("mongodb+srv://paligalavijay55:y1yBLDbRIYklYrZh@cluster0.dojhzeo.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Connected to MongoDB successfully!');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message);
+});
 
-module.exports = {mongoose};
+module.exports = { mongoose };
