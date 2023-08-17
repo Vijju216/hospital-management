b6c0e11d7c28afec40b585c8b9573b66cf85762a
Vijju216 <125346609+Vijju216@users.noreply.github.com>
2023-08-17 17:22:25 +0530

    Update sonar.js

--- sonar.js
+++ sonar.js
@@ -1,9 +1,9 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://34.212.181.13/:9000',
+  serverUrl: 'http://52.35.10.185:9000',
   options: {
-    'sonar.login': 'ae827d93f07eb38d75ff3342a09bab25169c9293',
+    'sonar.login': '1bfcd2cef99b3ba84e26dac79f643f179db50f6b',
     'sonar.sources': './',
     'sonar.exclusions': 'node_modules/**,public/**,views/**,test/**',
     'sonar.projectKey': 'nhs-app',
