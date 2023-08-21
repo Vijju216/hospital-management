153693679916de7dd5b7e3cd1f0182db82a706c8
Vijju216 <125346609+Vijju216@users.noreply.github.com>
2023-08-21 10:36:58 +0530

    Update sonar.js

--- sonar.js
+++ sonar.js
@@ -2,11 +2,18 @@ const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
   serverUrl: 'http://52.26.153.174:9000/',
-  options: {
-    'sonar.login': '224425d92c4e92075b85293f7bd2438890605c6c',
-    'sonar.sources': './',
-    'sonar.exclusions': 'node_modules/**,public/**,views/**,test/**',
-    'sonar.projectKey': 'nhs-app',
-    'sonar.projectName': 'NHS App',
-  },
-}, () => {});
+       options : {
+	    'sonar.projectDescription': 'This is a Node JS application',
+	    'sonar.projectName': 'Node JS Application - Sample',
+	    'sonar.projectKey':'NodeJsMithunTechnologies',
+	    'sonar.login': 'squ_224425d92c4e92075b85293f7bd2438890605c6c',
+	    //'sonar.login': 'admin',
+	    //'sonar.password': 'admin',
+            'sonar.projectVersion':'1.0',
+	    'sonar.language':'js',
+            'sonar.sourceEncoding':'UTF-8',
+            'sonar.sources': '.',
+	  //'sonar.tests': 'specs',
+          //'sonar.inclusions' : 'src/**'
+       },
+     }, () => {});
